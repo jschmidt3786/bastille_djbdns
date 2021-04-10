92b6835518164b054b693a09e10c7f515d072614
Jeff Schmidt <jeff.schmidt@zenith.com>
2021-04-09 23:34:32 -0500

    migrate from PRE to a bootstrap script

--- root/bootstrap.sh
+++ root/bootstrap.sh
@@ -1,4 +1,9 @@
-#!/usr/bin/env bash -x
+#!/usr/bin/env bash
+set -o errexit
+set -o nounset
+set -o pipefail
+
+[ -f /var/tmp/djbdns_fin ] && exit 0
 
 for U in dnslog tinydns axfrdns dnscache ; do
   pw useradd -n "$U" -c "$U daemon" -m -s /usr/sbin/nologin
@@ -10,13 +15,13 @@ chown -R dnslog:dnslog /var/log/dnslog
 echo "svscan_servicedir=/var/service" >> /etc/rc.conf
 ifconfig bastille0 |grep "inet " |awk '{print $2}' > /tmp/IP
 
-tinydns-conf tinydns dnslog /var/tinydns $(cat /tmp/IP)
+tinydns-conf tinydns dnslog /var/tinydns "$(cat /tmp/IP)"
 sed -i .bak s/^exec/#exec/ /var/tinydns/log/run
 echo "exec setuidgid dnslog multilog t s16777215 n100 /var/log/dnslog/tinydns" >> /var/tinydns/log/run
 rm -rf /var/tinydns/log/main /var/tinydns/log/run.bak
 ln -s /var/tinydns /var/service
 
-axfrdns-conf axfrdns dnslog /var/axfrdns /var/tinydns $(cat /tmp/IP)
+axfrdns-conf axfrdns dnslog /var/axfrdns /var/tinydns "$(cat /tmp/IP)"
 sed -i .bak s/^exec/#exec/ /var/axfrdns/log/run
 echo "exec setuidgid dnslog multilog t s16777215 n100 /var/log/dnslog/axfrdns" >> /var/axfrdns/log/run
 grep -v ^# /var/axfrdns/tcp
@@ -24,3 +29,4 @@ cd /var/axfrdns && make
 rm -rf /var/axfrdns/log/main /var/axfrdns/log/run.bak
 ln -s /var/axfrdns /var/service
 
+touch /var/tmp/djbdns_fin
